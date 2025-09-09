2e0dca65ce35c51cb30b92a04ec50e793864be5f
adansyah <adansyah225@gmail.com>
2025-09-09 11:36:56 +0700

    fisrt commit

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dashboard', function () {
-    return view('page.dashboard');
-});
+Route::get('dashboard', [HomeController::class, 'index']);
+
 
 Route::get('/akademik', function () {
     return view('page.akademik');
